ccc26a9b31bbf4f11d160589335b7db22d9485ac
facundo freires <facundof@clxstrzz.onmicrosoft.com>
2022-05-04 19:42:48 -0300

    prueba

--- FT-M3-master/02-Promises/ejemplos.js
+++ FT-M3-master/02-Promises/ejemplos.js
@@ -17,7 +17,7 @@ var segundoMetodo = function(datos) {
       setTimeout(function() {
          console.log('Terminó el segundo método');
          resolve('segundo');
-      }, 2000);
+      }, 20);
    });
    return promise;
 };
